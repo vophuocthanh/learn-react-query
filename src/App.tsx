bb7bcffdcabfcf6f9871f02be1bae238849318b9
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-01-07 22:40:00 +0700

    ammed(feat): add branch new (develop)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-import { useProfile } from '@/hooks'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
-const App: React.FC = () => {
-  const { data: userProfile, isLoading, isError, error } = useProfile()
-
+const Service: React.FC = () => {
+  const fetchProfile = async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    console.log('res:', res)
+    return res.data
+  }
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['user'],
+    queryFn: fetchProfile,
+    refetchOnWindowFocus: false,
+  })
   if (isLoading) {
-    return <div>Loading profile...</div>
+    return <div>Loading...</div>
   }
-
   if (isError) {
-    return <div>Error: {error.message}</div>
+    return <div>Error</div>
   }
-
   return (
     <div>
       <h1>User Profile</h1>
-      {userProfile && (
-        <div>
-          <p>Name: {userProfile.name}</p>
-          <p>Email: {userProfile.email}</p>
-        </div>
-      )}
+      {data?.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>Name: {item.name}</p>
+            <p>Email: {item.email}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
-export default App
+export default Service
